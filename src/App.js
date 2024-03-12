0a71cf47c273c0b92a92b200276e8d979bfd0176
krishna-kumarr <krishnakumar180320@gmail.com>
2024-03-12 11:56:19 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DashboardLayout from "./Component/Jsx_Files/DashboardLayout";
+import Portfolio from "./Component/Jsx_Files/Portfolio";
+import DiveDeep from "./Component/Jsx_Files/DiveDeep";
+import Analysis from "./Component/Jsx_Files/Analysis";
+import Login from "./Component/Jsx_Files/Login";
+import "./index.css"
+import Error404 from "./Component/Jsx_Files/Error404";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+
+          <Route path="/project" element={<DashboardLayout/>}>
+            <Route path="portfolio" element={<Portfolio />} />
+            <Route path="dive-deep" element={<DiveDeep/>}/>
+            <Route path="analysis" element={<Analysis/>}/>
+          </Route>
+
+          <Route path="*" element={<Error404 />}/>
+
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
